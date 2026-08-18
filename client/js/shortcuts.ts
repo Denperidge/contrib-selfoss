df7911d56ee5053938e434c394e48fe215f2baba
Denperidge <cat@denperidge.com>
2026-08-18 22:05:14 +0200

    client: created shortcuts help dialog
    Thanks to Jan for getting it to display using <dialog>!
    And dealing with a bunch of my questions.
    
    Co-authored-by: Jan Tojnar <jtojnar@gmail.com>

--- client/js/shortcuts.ts
+++ client/js/shortcuts.ts
@@ -1,6 +1,7 @@
 import { KeybindingsMap, tinykeys } from 'tinykeys';
 import selfoss from './selfoss-base';
 import { Direction } from './helpers/navigation';
+import { RefObject } from 'react';
 
 type KeyboardEventHandler = (event: KeyboardEvent) => void;
 
@@ -33,7 +34,7 @@ interface IKeybinding {
     action: KeyboardEventHandler;
 }
 
-const KEYBINDINGS: { [keycombo: string]: IKeybinding } = {
+export const KEYBINDINGS: { [keycombo: string]: IKeybinding } = {
     Space: {
         description: 'select and open next entry',
         action: () => {
@@ -178,13 +179,27 @@ const KEYBINDINGS: { [keycombo: string]: IKeybinding } = {
                 .click();
         },
     },
+    'h': {
+        description: "open shortcut help",
+        action: ()=>{}  // Assigned later
+    }
 };
 
-function makeKeybindingsMap(): KeybindingsMap {
+function makeKeybindingsMap(shortcutsDialog: RefObject<HTMLDialogElement>): KeybindingsMap {
     const shortcuts = Object.entries(KEYBINDINGS);
     const keybindingsMap: KeybindingsMap = {};
 
     for (const [keycombo, keybind] of shortcuts) {
+        if (keycombo == "h") {
+            keybind.action = () => {
+                if (shortcutsDialog.current.open) {
+                    shortcutsDialog.current.close();
+
+                } else {
+                    shortcutsDialog.current.showModal();
+                }
+            }
+        }
         keybindingsMap[keycombo] = ignoreWhenInteracting(
             (event: KeyboardEvent) => {
                 event.preventDefault();
@@ -198,6 +213,6 @@ function makeKeybindingsMap(): KeybindingsMap {
 /**
  * Set up shortcuts on document.
  */
-export default function makeShortcuts(): () => void {
-    return tinykeys(window, makeKeybindingsMap());
+export default function makeShortcuts(shortcutsDialog: RefObject<HTMLDialogElement>): () => void {
+    return tinykeys(window, makeKeybindingsMap(shortcutsDialog));
 }
